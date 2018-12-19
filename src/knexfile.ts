456de1fe2543e82a26359cdfe1af94f274dbc33b
Drake Bott <bott.da@gmail.com>
2018-12-19 15:00:54 -0700

    Create knex config file

---
+++ src/knexfile.ts
@@ -0,0 +1,32 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/spont_dev',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/spont_test',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/testing',
+    },
+    useNullAsDefault: true,
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  },
+};
