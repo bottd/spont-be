f150fad06690023a6de12c97a757c7e41b8735a1
Drake Bott <bott.da@gmail.com>
2018-12-20 13:52:51 -0700

    Add createUser mutation to schema

--- src/resolver.ts
+++ src/resolver.ts
@@ -4,5 +4,42 @@ const config = require('./knexfile')[environment];
 const database = knex(config);
 
 export const rootValue = {
-  users: () => database('users').select(),
+  user: async ({ id }) => {
+    const locations = await selectLocationsByUserID(id);
+    const user = await selectUserByID(id);
+    return { ...user, locations };
+  },
+  locations: () => database('locations').select(),
+  newUser: () => createNewUser(),
 };
+
+function selectUserByID(id) {
+  return database('users')
+    .where('id', id)
+    .first();
+}
+
+function selectLocationByID(id) {
+  return database('locations')
+    .where('id', id)
+    .first();
+}
+
+async function selectLocationsByUserID(id) {
+  const locationJoins = await database('user_locations')
+    .where('user_id', id)
+    .select();
+  const locations = locationJoins.map(join =>
+    selectLocationByID(join.location_id),
+  );
+  return Promise.all(locations);
+}
+
+async function createNewUser() {
+  const newUser = await database('users').insert({}, '*');
+  return newUser[0]
+}
+
+// function selectUsersByLocationID(id) {
+//   return database('locations').where('id', id).first();
+// }
