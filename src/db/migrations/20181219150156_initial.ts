86debbcea4712194f45d218885175ee5356b5df7
Drake Bott <bott.da@gmail.com>
2018-12-19 17:12:06 -0700

    Update db to do uuid keys automatically on insertion

--- src/db/migrations/20181219150156_initial.ts
+++ src/db/migrations/20181219150156_initial.ts
@@ -3,23 +3,24 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<any> {
   return Promise.all([
     knex.schema.createTable('users', (table) => {
-      table.uuid('id').primary();
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
       table.string('user_hash');
       table.timestamps(true, true);
     }),
     knex.schema.createTable('locations', (table) => {
-      table.uuid('id').primary();
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
       table.string('location_name');
       table.float('latitude');
       table.float('longitude');
       table.timestamps(true, true);
     }),
     knex.schema.createTable('user_locations', (table) => {
-      table.uuid('id').primary();
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
       table.uuid('user_id');
       table.foreign('user_id').references('users.id');
       table.uuid('location_id');
       table.foreign('location_id').references('locations.id');
+      table.integer('visit_count');
       table.timestamps(true, true);
     }),
   ]);
