4bca4c992ee472e451cb06b45a4070e539f45c58
Drake Bott <bott.da@gmail.com>
2018-12-22 13:52:28 -0700

    Remove unused testing env

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -10,17 +10,6 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
-  testing: {
-    client: 'pg',
-    connection: 'postgres://localhost/spont_test',
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds/testing',
-    },
-    useNullAsDefault: true,
-  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
