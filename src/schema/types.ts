527768ac9f13a79f5f509b46e0eb2f391f21a2e6
Drake Bott <bott.da@gmail.com>
2018-12-22 11:23:40 -0700

    Create user and location object types for schema

---
+++ src/schema/types.ts
@@ -0,0 +1,42 @@
+import * as graphql from 'graphql';
+import { selectLocationsByUserID, selectUsersByLocationID } from './utils';
+
+const {
+  GraphQLObjectType,
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLString,
+  GraphQLNonNull,
+} = graphql;
+
+export const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLString },
+    created_at: { type: GraphQLString },
+    updated_at: { type: GraphQLString },
+    locations: {
+      type: new GraphQLList(LocationType),
+      resolve(parentValue, args) {
+        return selectLocationsByUserID(parentValue.id);
+      },
+    },
+  }),
+});
+
+export const LocationType = new GraphQLObjectType({
+  name: 'Location',
+  fields: () => ({
+    id: { type: GraphQLString },
+    location_name: { type: GraphQLString },
+    category: { type: GraphQLString },
+    latitude: { type: GraphQLFloat },
+    longitude: { type: GraphQLFloat },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return selectUsersByLocationID(args.id);
+      },
+    },
+  }),
+});
