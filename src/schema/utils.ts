e8bfae90aed5030835219dc6e720e45afa50e4dc
Drake Bott <bott.da@gmail.com>
2018-12-25 10:34:17 -0700

    Modify knex import path

--- src/schema/utils.ts
+++ src/schema/utils.ts
@@ -1,6 +1,6 @@
 import * as knex from 'knex';
 const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile')[environment];
+const config = require('../../knexfile')[environment];
 const database = knex(config);
 
 export function selectUserByID(id: string) {
