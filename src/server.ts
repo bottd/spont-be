e29c9c317e1d694121daa6186c5c46b8c105be19
Drake Bott <bott.da@gmail.com>
2018-12-31 20:54:31 -0700

    Create post endpoint for posting lat/long coords to for background posting since HTTP requests won't fire in background

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 import { schema } from './schema';
+import { insertLocation, getLocationByCoords } from './schema/utils';
 
 require('dotenv').config();
 const app = express();
@@ -15,6 +16,28 @@ app.use(
     graphiql: true,
   }),
 );
+
+app.post('/locations', async (req, res) => {
+  const missingParams = [];
+  for (let requiredParam in ['latitude', 'longitude', 'userID']) {
+    if (!req.args[requiredParam]) {
+      missingParams.push(requiredParam);
+    }
+  }
+  if (missingParams.length) {
+    return res
+      .status(400)
+      .json({ message: `Missing required params of ${missingParams}` });
+  }
+
+  const locations = await getLocationByCoords(args.latitude, args.longitude);
+  if (locations.length) {
+    const join = await insertLocation(locations[0], { id: args.userID });
+    return res.status(200).json(join);
+  } else {
+    return res.status(200).json(args);
+  }
+});
 
 app.listen(app.get('port'), () => {
   console.log(`listening on port ${app.get('port')}`);
