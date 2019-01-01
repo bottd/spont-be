fe8943ae8bd688ad56828b0be8854e63372f8516
Drake Bott <bott.da@gmail.com>
2018-12-31 22:31:56 -0700

    Add production migration

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true',
     migrations: {
-      directory: './dist/migrations',
+      directory: './migrations',
     },
     seeds: {
       directory: './dist/seeds',
