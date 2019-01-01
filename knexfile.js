6e6f2e6737c685e9afe3cb2d6b229bc033c15640
Drake Bott <bott.da@gmail.com>
2018-12-31 18:54:14 -0700

    Add test environment to knex

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,17 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/spont_test',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/testing',
+    },
+    useNullAsDefault: true,
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + '?ssl=true',
